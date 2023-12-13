c1b1300c5267d2e7b762018f5660d27fc883a93c
Vitor Ferreira <vitorstick@gmail.com>
2023-12-13 14:57:34 +0100

    tailwind

--- apps/personal-page/src/app/app.component.ts
+++ apps/personal-page/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'vitor-workspace-root',
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent],
+  selector: 'vt-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- apps/personal-page/src/app/nx-welcome.component.ts
+++ apps/personal-page/src/app/nx-welcome.component.ts
@@ -1,8 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'vitor-workspace-nx-welcome',
+  selector: 'vt-nx-welcome',
   standalone: true,
   imports: [CommonModule],
   template: `
