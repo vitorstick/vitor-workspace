002b2443d02e75a7234686ba2d5f4f24f6ce589c
Vitor Ferreira <vitorstick@gmail.com>
2023-12-13 21:16:46 +0100

    template page

--- apps/personal-page/src/app/app.component.ts
+++ apps/personal-page/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { TemplateComponent } from './components/template/template.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, TemplateComponent],
   selector: 'vt-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
