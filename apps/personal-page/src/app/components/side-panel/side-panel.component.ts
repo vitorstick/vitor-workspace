424afcb6f37ff557b3d1b9d888701fc6965315a1
Vitor Ferreira <vitorstick@gmail.com>
2023-12-27 14:58:42 +0100

    using ng image optimizer from angular

--- apps/personal-page/src/app/components/side-panel/side-panel.component.ts
+++ apps/personal-page/src/app/components/side-panel/side-panel.component.ts
@@ -1,10 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'vt-side-panel',
   templateUrl: './side-panel.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage]
 })
 export class SidePanelComponent {
 }
